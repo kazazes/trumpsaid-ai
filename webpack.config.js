3005bb96477a07b86cc4977eeda92d50526c191b
Peter Kazazes <peter@peterk.co>
2018-07-29 07:09:56 -0700

    Make subdirectories for Vue apps

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,11 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
-  mode: "development",
   entry: {
     main: './src/public/js/main.js',
     main: './src/public/css/main.scss',
     admin: './src/public/css/admin.scss',
-    admin: './src/vue/main.js'
+    admin: './src/vue/AppAdmin/main.js'
   },
   output: {
     path: __dirname + '/dist/public/',
@@ -16,8 +15,6 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      // Options similar to the same options in webpackOptions.output
-      // both options are optional
       filename: "css/[name].css",
       chunkFilename: "css/[id].css",
     }),
@@ -28,7 +25,6 @@ module.exports = {
     new VueLoaderPlugin()
   ],
   resolve: {
-    // Add '.ts' and '.tsx' as a resolvable extension.
     extensions: ['.webpack.js', '.web.js', '.ts', '.tsx', '.js', '.scss', '.pug', '.jpg', '.png', '.svg', '.vue'],
     alias: {
       vue: 'vue/dist/vue.js'
