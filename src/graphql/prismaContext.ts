d665224e6efe021d8ca068dbe3c177b03a6be462
Peter Kazazes <peter@peterk.co>
2018-07-30 02:05:56 -0700

    Add prisma check to startup connection checks

--- src/graphql/prismaContext.ts
+++ src/graphql/prismaContext.ts
@@ -2,10 +2,14 @@ import { PrismaOptions } from 'prisma-binding/dist/types';
 import secrets from '../util/secrets';
 import { Prisma } from './generated/prisma';
 
+export const prismaEndpoint = secrets.PRISMA_ENDPOINT + (process.env.NODE_ENV === 'production' ? 'production' : 'development');
+
 const prismaOptions: PrismaOptions = {
   typeDefs: 'src/graphql/generated/prisma.graphql',
   secret: secrets.PRISMA_SECRET,
-  endpoint: secrets.PRISMA_ENDPOINT,
+  endpoint: prismaEndpoint,
 };
 
-export default new Prisma(prismaOptions);
+const prisma = new Prisma(prismaOptions);
+
+export default prisma;
