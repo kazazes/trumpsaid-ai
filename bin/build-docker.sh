72ab722c5b2004beafadc460f46586ea5ffe8c50
Peter Kazazes <peter@peterk.co>
2018-09-01 16:24:29 -0700

    drop docker support

--- bin/build-docker.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-set -a
-
-docker build -t app --cache-from gcr.io/summer-artwork-138223/app:latest -f server-slim.dockerfile .
-docker build -t worker --cache-from --cache-from gcr.io/summer-artwork-138223/worker:latest -f worker.dockerfile .
---
+++ bin/web-startup.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+export NODE_ENV=production
+
+cd /var/trumpsaid-wtf
+
+git pull
+chown -R node:web /var/trumpsaid-wtf
+
+sudo -u node yarn --pure-lockfile --prefer-offline
+sudo -u node yarn build
+cd /var/trumpsaid-wtf/packages/client/dist
+sudo -u node gsutil -m rsync -r -d gs://trumpsaid-wtf-static/web/ .
+cd /var/trumpsaid-wtf/packages/server
+sudo -u node gsutil cp gs://trumpsaid-wtf-static/env/web.env .env
+sudo -u node pm2 start dist/server.js --name ts-web
