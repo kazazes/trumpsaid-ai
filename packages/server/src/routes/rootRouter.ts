b8d14fdb560018417d343149bed4e2d3883869a3
Peter Kazazes <peter@peterk.co>
2018-09-09 11:01:43 -0400

    footer and privacy policy

--- packages/server/src/routes/rootRouter.ts
+++ packages/server/src/routes/rootRouter.ts
@@ -6,4 +6,8 @@ router.get('/', (_, res) => {
   res.render('home', { title: 'Home' });
 });
 
+router.get('/privacy', (_, res) => {
+  res.render('privacy', { title: 'Privacy Policy' });
+});
+
 export default router;
