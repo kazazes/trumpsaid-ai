cb97db680213bae2bb94e564540c94de7cfb4f90
Peter Kazazes <peter@peterk.co>
2018-09-10 05:38:57 -0400

    use vendor css in admin

--- packages/client/src/admin-app/views/VideoSubmissionInitialMetadata.vue
+++ packages/client/src/admin-app/views/VideoSubmissionInitialMetadata.vue
@@ -1,71 +1,38 @@
 <template>
   <b-row>
     <b-col md="8">
-      <VideoPlayer
-        id="master"
-        ref="masterPlayer"
-        :sources="getSource(getAMaster())"
-        preload="auto"
-        data-setup="{}"
-        @playerLoaded="setPlaceholderEnd"
-      />
+      <VideoPlayer id="master" ref="masterPlayer" :sources="getSource(getAMaster())" preload="auto" data-setup="{}" @playerLoaded="setPlaceholderEnd" />
     </b-col>
     <b-col md="4">
       <b-form-group>
         <b-form-group>
           <label for="numberOfSpeakers">Number of speakers in video:</label>
-          <b-form-input
-            id="numberOfSpeakers"
-            type="number"
-            :value="initialMetadata.numberOfSpeakers"
-          />
+          <b-form-input id="numberOfSpeakers" type="number" :value="initialMetadata.numberOfSpeakers" />
         </b-form-group>
         <b-form-group>
           <label for="thumbnailTimestamp">Thumbnail timestamp:</label>
           <b-input-group>
-            <b-form-input
-              id="thumbnailTimestamp"
-              :value="initialMetadata.thumbnailTimestamp"
-              disabled
-            />
+            <b-form-input id="thumbnailTimestamp" :value="initialMetadata.thumbnailTimestamp" disabled />
             <b-input-group-append>
-              <b-btn
-                variant="outline-success"
-                @click="setToPlayerTime('thumbnailTimestamp')"
-              >Set to player time</b-btn>
+              <b-btn variant="outline-success" @click="setToPlayerTime('thumbnailTimestamp')">Set to player time</b-btn>
             </b-input-group-append>
           </b-input-group>
         </b-form-group>
         <b-form-group>
           <label for="videoStartTimestamp">Video start:</label>
           <b-input-group>
-            <b-form-input
-              id="videoStartTimestamp"
-              disabled
-              :value="initialMetadata.renderStart"
-            />
+            <b-form-input id="videoStartTimestamp" disabled :value="initialMetadata.renderStart" />
             <b-input-group-append>
-              <b-btn
-                variant="outline-success"
-                @click="setToPlayerTime('renderStart')"
-              >Set to player time</b-btn>
+              <b-btn variant="outline-success" @click="setToPlayerTime('renderStart')">Set to player time</b-btn>
             </b-input-group-append>
           </b-input-group>
         </b-form-group>
         <b-form-group>
           <label for="videoEndTimestamp">Video end:</label>
           <b-input-group>
-            <b-form-input
-              id="videoEndTimestamp"
-              ref="videoEnd"
-              disabled
-              :value="initialMetadata.renderEnd"
-            />
+            <b-form-input id="videoEndTimestamp" ref="videoEnd" disabled :value="initialMetadata.renderEnd" />
             <b-input-group-append>
-              <b-btn
-                variant="outline-success"
-                @click="setToPlayerTime('renderEnd')"
-              >Set to player time</b-btn>
+              <b-btn variant="outline-success" @click="setToPlayerTime('renderEnd')">Set to player time</b-btn>
             </b-input-group-append>
           </b-input-group>
         </b-form-group>
@@ -77,137 +44,137 @@
   </b-row>
 </template>
 <script lang="ts">
-import timestampFormat from 'hh-mm-ss';
-import Vue from 'vue';
-import {
-  VideoUpload,
-  VideoUploadStorageLink,
-} from '@trumpsaid/prisma';
-import { SET_INITIAL_UPLOAD_METADATA } from '../constants/graphql';
-import VideoPlayer from './VideoPlayer.vue';
-import Component from 'vue-class-component';
+  import {
+    VideoUpload,
+    VideoUploadStorageLink,
+  } from '@trumpsaid/prisma';
+  import timestampFormat from 'hh-mm-ss';
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+  import { SET_INITIAL_UPLOAD_METADATA } from '../constants/graphql';
+  import VideoPlayer from './VideoPlayer.vue';
 
-@Component({
-  name: 'VideoSubmissionInitialMetadata',
-  components: { VideoPlayer },
-  props: { videoUpload: Object as () => VideoUpload },
-  apollo: {
-    setInitialMetadata: {
-      query: SET_INITIAL_UPLOAD_METADATA,
-      variables() {
-        return {
+  @Component({
+    name: 'VideoSubmissionInitialMetadata',
+    components: { VideoPlayer },
+    props: { videoUpload: Object as () => VideoUpload },
+    apollo: {
+      setInitialMetadata: {
+        query: SET_INITIAL_UPLOAD_METADATA,
+        variables() {
+          return {
+            id: this.videoUpload.id,
+            thumbnailTimestamp: this.thumbnailTimestampInSeconds,
+            renderStart: this.renderStartInSeconds,
+            renderEnd: this.renderEndInSeconds,
+          };
+        },
+        skip: true,
+      },
+    },
+  })
+  export default class VideoSubmissionInitialMetadata extends Vue {
+    private videoUpload: VideoUpload;
+    private initialMetadata: any = {
+      renderStart: '00:00',
+      renderEnd: '00:00',
+      thumbnailTimestamp: '',
+      numberOfSpeakers: 2,
+    };
+    get renderStartInSeconds() {
+      return timestampFormat.toMs(this.initialMetadata.renderStart) / 1000;
+    }
+    get renderEndInSeconds() {
+      return timestampFormat.toMs(this.initialMetadata.renderEnd) / 1000;
+    }
+
+    get thumbnailTimestampInSeconds() {
+      return timestampFormat.toMs(this.initialMetadata.thumbnailTimestamp) / 1000;
+    }
+
+    get player() {
+      return (this.$refs.masterPlayer as VideoPlayer).player;
+    }
+
+    private submitInitialMetadata() {
+      if (this.renderStartInSeconds > this.renderEndInSeconds) {
+        return this.$notify({
+          type: 'error',
+          title: 'Video start must be before video end...',
+        });
+      }
+
+      if (
+        !(
+          this.renderEndInSeconds !== undefined &&
+          this.renderStartInSeconds !== undefined &&
+          this.thumbnailTimestampInSeconds !== undefined
+        )
+      ) {
+        return this.$notify({
+          type: 'error',
+          title: 'Thumbnail, start and end time are required!',
+        });
+      }
+
+      this.$apollo.mutate({
+        mutation: SET_INITIAL_UPLOAD_METADATA,
+        variables: {
           id: this.videoUpload.id,
           thumbnailTimestamp: this.thumbnailTimestampInSeconds,
           renderStart: this.renderStartInSeconds,
           renderEnd: this.renderEndInSeconds,
-        };
-      },
-      skip: true,
-    },
-  },
-})
-export default class VideoSubmissionInitialMetadata extends Vue {
-  videoUpload: VideoUpload;
-  initialMetadata: any = {
-    renderStart: '00:00',
-    renderEnd: '00:00',
-    thumbnailTimestamp: '',
-    numberOfSpeakers: 2,
-  };
-  get renderStartInSeconds() {
-    return timestampFormat.toMs(this.initialMetadata.renderStart) / 1000;
-  }
-  get renderEndInSeconds() {
-    return timestampFormat.toMs(this.initialMetadata.renderEnd) / 1000;
-  }
-
-  get thumbnailTimestampInSeconds() {
-    return timestampFormat.toMs(this.initialMetadata.thumbnailTimestamp) / 1000;
-  }
-
-  get player() {
-    return (this.$refs.masterPlayer as VideoPlayer).player;
-  }
-
-  submitInitialMetadata() {
-    if (this.renderStartInSeconds > this.renderEndInSeconds) {
-      return this.$notify({
-        type: 'error',
-        title: 'Video start must be before video end...',
+          numberOfSpeakers: this.initialMetadata.numberOfSpeakers,
+        },
       });
     }
-
-    if (
-      !(
-        this.renderEndInSeconds !== undefined &&
-        this.renderStartInSeconds !== undefined &&
-        this.thumbnailTimestampInSeconds !== undefined
-      )
-    ) {
-      return this.$notify({
-        type: 'error',
-        title: 'Thumbnail, start and end time are required!',
-      });
+    private setPlaceholderEnd() {
+      this.initialMetadata.renderEnd = timestampFormat.fromS(
+        this.player.duration(),
+        'hh:mm:ss'
+      );
     }
-
-    this.$apollo.mutate({
-      mutation: SET_INITIAL_UPLOAD_METADATA,
-      variables: {
-        id: this.videoUpload.id,
-        thumbnailTimestamp: this.thumbnailTimestampInSeconds,
-        renderStart: this.renderStartInSeconds,
-        renderEnd: this.renderEndInSeconds,
-        numberOfSpeakers: this.initialMetadata.numberOfSpeakers,
-      },
-    });
-  }
-  setPlaceholderEnd() {
-    this.initialMetadata.renderEnd = timestampFormat.fromS(
-      this.player.duration(),
-      'hh:mm:ss'
-    );
-  }
-  formatedUpload() {
-    return JSON.stringify(this.videoUpload, null, 2);
-  }
-  getPoster(video: VideoUpload) {
-    const storageLink = video.storageLinks.find(
-      (link) => link.version === 'WEB' && link.fileType === 'THUMBNAIL'
-    );
-    const linkUrl = `https://storage.googleapis.com/${
-      storageLink.bucket
-      }/${encodeURI(storageLink.path)}`;
-    return linkUrl;
-  }
-  setToPlayerTime(prop: string) {
-    if (!this.player.paused()) {
-      return this.$notify({
-        type: 'warn',
-        title: 'Pause video',
-        text: 'You must pause the video first.',
-      });
+    private formatedUpload() {
+      return JSON.stringify(this.videoUpload, null, 2);
     }
-
-    this.initialMetadata[prop] = timestampFormat.fromS(
-      this.player.currentTime(),
-      'hh:mm:ss'
-    );
-  }
-  getSource(storageLink: VideoUploadStorageLink) {
-    const linkUrl = `https://storage.googleapis.com/${
-      storageLink.bucket
-      }/${encodeURI(storageLink.path)}`;
-    return [{ src: linkUrl, mimeType: storageLink.mimeType }];
-  }
-  getAMaster(): VideoUploadStorageLink {
-    return this.videoUpload.storageLinks.find(
-      (link: VideoUploadStorageLink) => {
-        return (
-          link.version === 'MASTER' &&
-          (link.fileType === 'MP4' || link.fileType === 'WEBM')
-        );
+    private getPoster(video: VideoUpload) {
+      const storageLink = video.storageLinks.find(
+        (link) => link.version === 'WEB' && link.fileType === 'THUMBNAIL'
+      );
+      const linkUrl = `https://storage.googleapis.com/${
+        storageLink.bucket
+        }/${encodeURI(storageLink.path)}`;
+      return linkUrl;
+    }
+    private setToPlayerTime(prop: string) {
+      if (!this.player.paused()) {
+        return this.$notify({
+          type: 'warn',
+          title: 'Pause video',
+          text: 'You must pause the video first.',
+        });
       }
-    );
+
+      this.initialMetadata[prop] = timestampFormat.fromS(
+        this.player.currentTime(),
+        'hh:mm:ss'
+      );
+    }
+    private getSource(storageLink: VideoUploadStorageLink) {
+      const linkUrl = `https://storage.googleapis.com/${
+        storageLink.bucket
+        }/${encodeURI(storageLink.path)}`;
+      return [{ src: linkUrl, mimeType: storageLink.mimeType }];
+    }
+    private getAMaster(): VideoUploadStorageLink {
+      return this.videoUpload.storageLinks.find(
+        (link: VideoUploadStorageLink) => {
+          return (
+            link.version === 'MASTER' &&
+            (link.fileType === 'MP4' || link.fileType === 'WEBM')
+          );
+        }
+      );
+    }
   }
-}
 </script>
--- packages/client/src/admin-app/views/VideoSubmissionNewsSources.vue
+++ packages/client/src/admin-app/views/VideoSubmissionNewsSources.vue
@@ -3,89 +3,67 @@
     <b-col>
       <b-form-group>
         <div>
-          <b-btn
-            v-b-modal.newSourceModal
-            size="sm"
-            class="mx-2"
-            variant="primary"
-          ><i class="icon icon-plus" />&nbsp;Add source</b-btn>
-          <b-modal
-            id="newSpeakerModal"
-            ref="newSourceModal"
-            size="sm"
-            hide-header
-            centered
-            @ok="handleAddSourceModal"
-          >
-            <form @submit.stop.prevent="handleAddSourceSubmit">
+          <b-btn v-b-modal.newSourceModal size="sm" class="mx-2" variant="primary">
+            <i class="icon icon-plus" />&nbsp;Add source
+          </b-btn>
+          <b-modal id="newSourceModal" ref="newSourceModal" size="sm" hide-header centered @ok="handleAddSourceModal">
+            <form @submit.stop.prevent>
               <label for="speakerName">News source URL:</label>
-              <b-form-input
-                id="sourceUrlInput"
-                v-model="sourceUrlInput"
-                type="text"
-              />
+              <b-form-input id="sourceUrlInput" v-model="sourceUrlInput" type="text" />
             </form>
           </b-modal>
         </div>
-        <div
-          v-if="editableUpload.metadata.newsSources.length === 0"
-          class="text-center"
-        >
+        <div v-if="editableUpload.metadata.newsSources.length === 0" class="text-center">
           <h4 class="text-muted my-4">No News Sources</h4>
         </div>
-        <div
-          v-else
-          class="text-center"
-        >
-          <b-btn
-            variant="primary"
-            @click="handleSaveSources"
-          >Save</b-btn>
+        <div v-else class="text-center">
+          <b-btn variant="primary" @click="handleSaveSources">Save</b-btn>
         </div>
       </b-form-group>
     </b-col>
   </b-row>
 </template>
 <script lang="ts">
-import Vue from 'vue';
-import { UPDATE_METADATA } from '../constants/graphql';
+  import Vue from 'vue';
+  import { UPDATE_METADATA } from '../constants/graphql';
 
-export default Vue.extend({
-  name: 'VideoSubmissionMetadata',
-  components: {},
-  props: {
-    videoUpload: Object,
-  },
-  data() {
-    return {
-      editableUpload: { metadata: { newsSources: [] } },
-      sourceUrlInput: '',
-    };
-  },
-  mounted() {
-    this.editableUpload = JSON.parse(JSON.stringify(this.videoUpload));
-    this.$forceUpdate();
-  },
-  methods: {
-    handleAddSourceSubmit() {},
-    handleAddSourceModal() {},
-    async handleSaveSources() {
-      const update = {};
-
-      await this.$apollo.mutate({
-        mutation: UPDATE_METADATA,
-        variables: {
-          id: this.videoUpload.id,
-          metadata: update,
-        },
-      });
-
-      return this.$notify({
-        type: 'success',
-        title: 'Saved sources!',
-      });
+  export default Vue.extend({
+    name: 'VideoSubmissionMetadata',
+    components: {},
+    props: {
+      videoUpload: Object,
     },
-  },
-  apollo: {},
-});
+    data() {
+      return {
+        editableUpload: { metadata: { newsSources: [] } },
+        sourceUrlInput: '',
+      };
+    },
+    mounted() {
+      this.editableUpload = JSON.parse(JSON.stringify(this.videoUpload));
+      this.$forceUpdate();
+    },
+    methods: {
+      handleAddSourceModal() {
+        console.log(this.sourceUrlInput);
+      },
+      async handleSaveSources() {
+        const update = {};
+
+        await this.$apollo.mutate({
+          mutation: UPDATE_METADATA,
+          variables: {
+            id: this.videoUpload.id,
+            metadata: update,
+          },
+        });
+
+        return this.$notify({
+          type: 'success',
+          title: 'Saved sources!',
+        });
+      },
+    },
+    apollo: {},
+  });
 </script>
