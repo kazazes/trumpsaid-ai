32a5729c4c48dc196a78c77841ca950bdc03a2cf
Peter Kazazes <peter@peterk.co>
2018-09-14 23:09:13 -0400

    fix static upload path

--- bin/deploy-static.sh
+++ bin/deploy-static.sh
@@ -4,6 +4,6 @@ source .env
 
 set -x
 
-gsutil -m rsync -r -d packages/client/dist/ gs://trumpsaid-wtf-static/web/
+gsutil -m rsync -r -d packages/client/dist gs://trumpsaid-wtf-static/web/
 gsutil cp packages/server/.env.gce gs://trumpsaid-wtf-static/env/web.env
 gcloud compute scp --compress --recurse packages/client/dist/* $PRIMARY_LB_CONNECTION_STRING:/var/www/static/
